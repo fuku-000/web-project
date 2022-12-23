d8f496823618ad26e4e962ef5896df607383b7a3
fuku-000 <s1F102201188@iniad.org>
2022-12-23 16:13:53 +0900

    yohoho

--- webweb/urls.py
+++ webweb/urls.py
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('', views.root, name='root'),
-=======
 ##urls.pyはデフォルトでは作成されていないので作成すること。
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
->>>>>>> f7f2cfa8a6bd1d400f3aa9fd617b11705725410a
+
 ]
