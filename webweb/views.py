0ef0c4b34b1edf11a114ec6809143509b6733948
konjikun <shintaniallen@gmail.com>
2023-01-05 23:51:28 +0900

    投稿完了まで

--- webweb/views.py
+++ webweb/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 from webweb.forms import WebwebForm
+from django.views.generic import CreateView
+
  
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-class DiaryCreateView(CreateView):
-    template_name = 'diary_create.html'
+class WebwebCreateView(CreateView):
+    template_name = 'webweb_create.html'
     form_class = WebwebForm
-    success_url = reverse_lazy('diary:diary_create_complete')
+    success_url = reverse_lazy('webweb:webweb_create_complete')
 
-class DiaryCreateCompleteView(TemplateView):
-    template_name = 'diary_create_complete.html'
+class WebwebCreateCompleteView(TemplateView):
+    template_name = 'webweb_create_complete.html'
