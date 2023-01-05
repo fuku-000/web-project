e46529c367d4782c53f872ee9c7111bdd0279f92
s1f10220214 <s1f102202145@iniad.org>
2023-01-05 22:38:45 +0900

    views.py

--- webweb/views.py
+++ webweb/views.py
@@ -3,3 +3,23 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, '../templates/webweb/main/index.html')
+
+from webweb.models import Article
+from django.http import Http404
+
+def index(request):
+    context={
+        "articles":Article.objects.all()
+    }
+    return render(request, 'blog/index.html',context)
+
+def index(request):
+    if request.method == 'POST':
+        article = Article(title=request.POST['title'],body=request.POST['text'])
+        article.save()
+        return redirect(detail, article.id)
+
+        context = {
+            "articles": Article.object.all()
+        }
+        return render(request, 'Blog/index.html',context)
