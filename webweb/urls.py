d01587bfbe16f9f946179b56f57edcb0ce316c8d
fuku-000 <s1F102201188@iniad.org>
2023-01-06 10:19:34 +0900

    Update urls.py

--- webweb/urls.py
+++ webweb/urls.py
@@ -1,1 +1,11 @@
 
+from django.urls import path
+from . import views
+ 
+app_name = 'webweb'
+urlpatterns = [
+    path('index/', views.IndexView.as_view(), name='index'),
+    path('create/', views.WebwebCreateView.as_view(), name='webweb_create'),
+    path('complete/', views.WebwebCreateCompleteView.as_view(), name='webweb_create_complete'),
+]
+
